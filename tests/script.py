352065f413115c8737000badce1acdb4ea9530d7
GrandChacalDominant <91677699+merpo686@users.noreply.github.com>
2024-10-04 15:14:58 +0200

    first commit test

--- tests/script.py
+++ tests/script.py
@@ -1,21 +1,19 @@
-import numpy as np
 import pandas as pd
 
 
-path_users = "data/PP_users.csv"
-df_users = pd.read_csv(path_users, sep=',')
+PATH_USERS = "data/PP_users.csv"
+df_users = pd.read_csv(PATH_USERS, sep=',')
 # print(df_users.info())
 
-path_recipes = "data/PP_recipes.csv"
-df_recipes = pd.read_csv(path_recipes, sep=',')
+PATH_RECIPES = "data/PP_recipes.csv"
+df_recipes = pd.read_csv(PATH_RECIPES, sep=',')
 print(df_recipes.info())
 # print(df_recipes.head())
 
-path_recipes_raw = "data/RAW_recipes.csv"
-df_recipes_raw = pd.read_csv(path_recipes_raw, sep=',')
+PATH_RECIPES_RAW = "data/RAW_recipes.csv"
+df_recipes_raw = pd.read_csv(PATH_RECIPES_RAW, sep=',')
 print(df_recipes_raw.info())
 # print(df_recipes_raw.head())
 
-print(df_recipes["ingredient_ids"][df_recipes["id"]==424415])
-print(df_recipes_raw["ingredients"][df_recipes_raw["id"]==424415])
-
+print(df_recipes["ingredient_ids"][df_recipes["id"] == 424415])
+print(df_recipes_raw["ingredients"][df_recipes_raw["id"] == 424415])
